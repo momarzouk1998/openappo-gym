f629009abf463cc62fbf2c0297301394a96b0101
sohbatalquran411 <sohbatalquran411@gmail.com>
2026-06-26 06:54:13 +0300

    fix: enable trustHost for NextAuth behind nginx reverse proxy

--- lib/auth.ts
+++ lib/auth.ts
@@ -5,6 +5,7 @@ import bcrypt from 'bcryptjs'
 import type { NextAuthConfig } from 'next-auth'
 
 export const authConfig: NextAuthConfig = {
+  trustHost: true,
   pages: {
     signIn: '/login',
   },
